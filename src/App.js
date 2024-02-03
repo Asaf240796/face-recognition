a831bb9944fd3b8303e8432e6ea81c82a6756f91
Asaf Elazar <Asaf.Elazar@cellebrite.com>
2024-02-03 12:10:34 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,86 @@ import Navigation from "./components/Navigation/Navigation.js";
 import Logo from "./components/Logo/Logo.js";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank.js";
-import React from "react";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition.js";
+import React, { useState } from "react";
 import ParticlesBg from "particles-bg";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [boxBorder, setBoxBorder] = useState({});
+
+  const calculateFacePosition = (data) => {
+    const clarifiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifiFace.left_col * width,
+      topRow: clarifiFace.top_row * height,
+      rightCol: width - clarifiFace.right_col * width,
+      bottomRow: height - clarifiFace.bottom_row * height,
+    };
+  };
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const returnClarifiRequestOptions = (imageUrl) => {
+    const PAT = "2af950e61d6c4acb80551760947a079a";
+    const USER_ID = "35h1h6xbhrmp";
+    const APP_ID = "test";
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    return requestOptions;
+  };
+
+  const onBtnSubmit = () => {
+    fetch(
+      "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+      returnClarifiRequestOptions(input)
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const boundingBox =
+          result.outputs[0]?.data?.regions[0]?.region_info?.bounding_box;
+        if (boundingBox) {
+          console.log(boundingBox);
+          const facePosition = calculateFacePosition(result);
+          console.log(facePosition);
+        } else {
+          console.log("No face detected");
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div className="App">
       <ParticlesBg
@@ -17,9 +93,15 @@ function App() {
         num={200}
       />
       <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
+      <div>
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={onInputChange}
+          onBtnSubmit={onBtnSubmit}
+        />
+        <FaceRecognition imageUrl={input} box={boxBorder} />
+      </div>
     </div>
   );
 }
